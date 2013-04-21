54d9867210dae32a355fe39fe7d1ed02787044ff
AndreasSandberg <andreasfrey@hotmail.com>
2013-04-21 20:34:54 +0200

    New background color and bug fix for one character.

--- RuzzleResolver/src/se/sandberg/ruzzleresolver/activities/PathActivity.java
+++ RuzzleResolver/src/se/sandberg/ruzzleresolver/activities/PathActivity.java
@@ -27,7 +27,8 @@ public class PathActivity extends Activity {
 		//Set a pre draw listener which will be called when the layout pass is completed and the view is drawn
 		relativeLayout.getViewTreeObserver().addOnPreDrawListener(
 				new ViewTreeObserver.OnPreDrawListener() {
-
+					
+					@Override
 					public boolean onPreDraw() {
 						if(drawCompleted){
 							return true;
@@ -35,14 +36,11 @@ public class PathActivity extends Activity {
 						@SuppressWarnings("unchecked")
 						ArrayList<CharIndex> indexes = (ArrayList<CharIndex>) getIntent().getExtras().getSerializable(this.getClass().getPackage().toString()+".result");
 						String[][] game = ((GamePlan) getIntent().getExtras().getParcelable(this.getClass().getPackage().toString()+".game")).getGame();
-						//No need to add lines between characters if one or less characters to show.
-						if(indexes.size() <= 1){
-							return true;
-						}
+
 						//final float[] coordinates = new float [(indexes.size()-1)*4];
 						List<Float> coordinates = new ArrayList<Float>();
 
-						for(int i = 0; i < indexes.size() ; i++){
+						for(int i = 0; i < indexes.size(); i++){
 							CharIndex charIndex = indexes.get(i);
 							int indexHorisontal = charIndex.getIndexHorisontal();
 							int indexVertical = charIndex.getIndexVertical();
@@ -53,40 +51,47 @@ public class PathActivity extends Activity {
 							characterEdit.setBackgroundResource(R.drawable.yellow_rounded_edittext);
 							characterEdit.setText(character);
 
+							//No need to extract coordinates when only one character is visible
+							if(indexes.size() == 1){
+								continue;
+							}
 							//Extract the coordinates for the edittext to draw lines between the characters.
-						    boolean firstOrLast = i == 0 || i == indexes.size() - 1;
+							boolean firstOrLast = i == 0 || i == indexes.size() - 1;
 							extractCoordinates(coordinates, characterEdit, firstOrLast);
 						}
+
+						//No need to add lines between characters if one or less characters to show.
+						if(!coordinates.isEmpty()){
+							RelativeLayout relativeLayout = (RelativeLayout) findViewById(R.id.pathLayout);
+							relativeLayout.addView(new LineView(getApplicationContext(), toArray(coordinates)));
+						}
 						
-						RelativeLayout relativeLayout = (RelativeLayout) findViewById(R.id.pathLayout);
-						relativeLayout.addView(new LineView(getApplicationContext(), toArray(coordinates)));
 						drawCompleted = true;
 						return true;
 					}
-
-					private void extractCoordinates(List<Float> coordinates, EditText characterEdit, boolean firstOrLast) {
-						int[] editTextCoordinates = new int[2];
-						characterEdit.getLocationOnScreen(editTextCoordinates);
-						coordinates.add((float) editTextCoordinates[0] + characterEdit.getWidth() / 2);
-						coordinates.add((float) editTextCoordinates[1]);
-
-						//First and last character only needs one coordinate pair
-						if(!firstOrLast){
-							coordinates.add((float) editTextCoordinates[0] + characterEdit.getWidth() / 2);
-							coordinates.add((float) editTextCoordinates[1]);
-						}
-					}
-					
-					private float[] toArray(List<Float> floats){
-						float[] result = new float[floats.size()];
-						for(int i = 0; i < floats.size(); i++){
-							result[i] = floats.get(i);
-						}
-						return result;
-					}					
 				});
 	}
+	
+	private void extractCoordinates(List<Float> coordinates, EditText characterEdit, boolean firstOrLast) {
+		int[] editTextCoordinates = new int[2];
+		characterEdit.getLocationOnScreen(editTextCoordinates);
+		coordinates.add((float) editTextCoordinates[0] + characterEdit.getWidth() / 2);
+		coordinates.add((float) editTextCoordinates[1]);
 
+		//First and last character only needs one coordinate pair
+		if(!firstOrLast){
+			coordinates.add((float) editTextCoordinates[0] + characterEdit.getWidth() / 2);
+			coordinates.add((float) editTextCoordinates[1]);
+		}
+	}
+
+	private float[] toArray(List<Float> floats){
+		float[] result = new float[floats.size()];
+		for(int i = 0; i < floats.size(); i++){
+			result[i] = floats.get(i);
+		}
+		return result;
+	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
